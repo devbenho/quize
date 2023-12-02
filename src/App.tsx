dec05cf30c4dff4ece8370998370567da8834801
Muhammed Hamdy <modclover7@gmail.com>
2023-12-02 23:27:28 +0200

    add page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
+import { QuizQuestion } from "./components/quiz-question";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/dev" element={<QuizQuestion />} />
         </Routes>
       </BrowserRouter>
     </div>
